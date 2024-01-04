166a1a4ad461090437f7299e12a17ac7b280645d
Andrew Pierno <andrewpierno@gmail.com>
2024-01-04 10:04:37 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# Video Summary SDK
+
+Elevate your video processing capabilities with the `Video Summary SDK`. This Node.js SDK provides a seamless way to transcribe, summarize, and extract chapter information from videos. Whether it's a local file or a hosted URL, `Video Summary SDK` handles it all with ease.
+
+## Key Features
+
+- **Transcription**: Convert speech in your videos into accurate text.
+- **Summarization**: Get concise summaries of video content.
+- **Chapter Extraction**: Identify and extract distinct chapters from videos.
+- **Support for Various Sources**: Works with local files, URLs, and S3 buckets.
+
+## Installation
+
+Install the package with npm:
+
+```bash
+npm install video-summary
+```
+
+## Getting Started 
+
+
+- Grab your api key for free at [https://videosummary.io](https://videosummary.io?utm_source=github) then follow the instructions below.
+
+```js
+const VideoSummarySDK = require('video-summary')
+const VideoSummary = new VideoSummarySDK('your api key')
+
+setTimeout(async () => {
+  try {
+    // local file works, s3 url works, signed s3 url works
+    const path = './your/local/file.mp4'
+    const res = await VideoSummary.summarize(path)
+    console.log("video summary res:", res)
+  } catch (e) {
+    console.error('video summary sdk e', e)
+  }
+})
+```
+
+## API Methods
+
+The SDK offers several methods to interact with your videos:
+
+#### Note 
+The id and callbackUrl are optional. the id field can be used for your own reference if you have as asset id already. the callbackUrl is used to send a webhook when the video is processed and sends the id back to you.
+
+### `summarize(url, [id], [callbackUrl])`
+Summarizes the video content. Provide the URL of the video, and optionally, an ID and a callback URL for asynchronous processing. 
+
+### `transcribe(url, [id], [callbackUrl])`
+Transcribes the audio content of the video into text. Pass the video URL, and if needed, an ID and a callback URL.
+
+### `chapter(url, [id], [callbackUrl])`
+Extracts chapters from the video for easy navigation and understanding. Requires the video URL, with optional ID and callback URL.
+
+### `summarizeAndChapter(url, [id], [callbackUrl])`
+Performs both summarization and chapter extraction on the video. Input the video URL, and optionally, an ID and a callback URL.
+
+Each method returns a promise that resolves to an object with relevant data about the video processing, including transcripts, summaries, chapters, and file IDs.
+
+
+## output
+```json 
+{
+  "transcript": {
+    "speakers": [
+      {
+      "speaker": "SPEAKER_00",
+      "text": " video",
+      "timestamp": [10,10.26],
+      "start": 10,
+      "end": 10.26
+    },
+    {
+      "speaker": "SPEAKER_00",
+      "text": " products.",
+      "timestamp": [10.26,10.9],
+      "start": 10.26,
+      "end": 10.9
+    }
+    ],
+    "chunks": [
+      { "text": " video", "timestamp": [Array] },
+      { "text": " products", "timestamp": [Array] },
+    ],
+    "text": "..."
+  },
+  "chapters": [
+    {
+      "start": 0,
+      "end": 10.9,
+      "title": "Introduction to VideoSummary.io",
+      "text": "Introducing videosummary.io. a simple api to transcribe, chapter and summarize audio and video files."
+    }
+  ],
+  "summary": "Developers, check this out. You need VideoSummary.io in your life. It lets you build video products much easier with features like video summarization and video chaptering. Grab it now and start building game-changing video products.",
+  "fileId": "xxx-xxx-4ffc-a2a5-13d3cee085dd"
+}
+```
---
+++ index.js
@@ -0,0 +1,238 @@
+const config = require('./config.json')
+const fs = require('fs')
+const axios = require('axios');
+const baseUrl = config.baseUrl
+
+const getUrlType = (url) => {
+  // if it's a file, a local path, or a url 
+  if (url.includes('http')) return 'url'
+  if (typeof File !== 'undefined' && source instanceof File) return 'file'
+  return 'path'
+}
+
+class VideoSummarySDK {
+  constructor(apiKey, _baseUrl = null) {
+    this.apiKey = apiKey;
+    this.baseUrl = _baseUrl || baseUrl;
+  }
+
+  async transcribe(url, id = null, callbackUrl = null) {
+    if (!url) return { error: 'url is required' }
+    const fileType = getUrlType(url)
+    const endpoint = `${this.baseUrl}/v1/transcribe`;
+    let res = null
+    if (fileType === 'url') {
+      res = await this._postRequest(endpoint, { url, id, callback: callbackUrl }, 'transc');
+    } else if (fileType === 'file') {
+      return { error: 'unimplemented file' }
+    } else if (fileType === 'path') {
+      const uploaded_url = await this._handle_local_file(url)
+      if (uploaded_url?.error) return uploaded_url
+      res = await this._postRequest(endpoint, { external_url: false, url: uploaded_url, id, callback: callbackUrl });
+    }
+    if (res?.file?.transcript) {
+      const json = await fetch(res?.file?.transcript)
+      const transcript = await json.json()
+      return { transcript, fileId: res.file.id }
+    }
+  }
+
+  async chapter(url, id = null, callbackUrl = null) {
+    if (!url) return { error: 'url is required' }
+    const fileType = getUrlType(url)
+    const endpoint = `${this.baseUrl}/v1/summary`;
+    let res = null
+    if (fileType === 'url') {
+      res = await this._postRequest(endpoint, {
+        url,
+        external_url: false,
+        chapter: true,
+        summarize: false,
+        id,
+        callback: callbackUrl
+      });
+    } else if (fileType === 'file') {
+      return { error: 'unimplemented file' }
+    } else if (fileType === 'path') {
+      const uploaded_url = await this._handle_local_file(url)
+      if (uploaded_url?.error) return uploaded_url
+      res = await this._postRequest(endpoint, {
+        external_url: false,
+        url: uploaded_url,
+        id,
+        chapter: true,
+        summarize: false,
+        callback: callbackUrl
+      });
+    }
+    let transcript = null
+    if (res?.file?.transcript) {
+      const json = await fetch(res?.file?.transcript)
+      transcript = await json.json()
+    }
+    let chapters = null
+    if (res?.file?.chaptering) {
+      chapters = res?.file?.chaptering
+    }
+    return { transcript, chapters, fileId: res.file.id }
+  }
+
+  async summarizeAndChapter(url, id = null, callbackUrl = null) {
+    if (!url) return { error: 'url is required' }
+    const fileType = getUrlType(url)
+    const endpoint = `${this.baseUrl}/v1/summary`;
+    let res = null
+    if (fileType === 'url') {
+      res = await this._postRequest(endpoint, {
+        url,
+        chapter: true,
+        summarize: true,
+        id,
+        callback: callbackUrl
+      });
+    } else if (fileType === 'file') {
+      return { error: 'unimplemented file' }
+    } else if (fileType === 'path') {
+      const uploaded_url = await this._handle_local_file(url)
+      if (uploaded_url?.error) return uploaded_url
+      res = await this._postRequest(endpoint, {
+        external_url: false,
+        url: uploaded_url,
+        id,
+        chapter: true,
+        summarize: true,
+        callback: callbackUrl
+      });
+    }
+    let transcript = null
+    if (res?.file?.transcript) {
+      const json = await fetch(res?.file?.transcript)
+      transcript = await json.json()
+    }
+    let chapters = null
+    if (res?.file?.chaptering) {
+      chapters = res?.file?.chaptering
+    }
+    return { transcript, chapters, summary: res?.file?.final_summary, fileId: res.file.id }
+  }
+
+  async summarize(url, id = null, callbackUrl = null) {
+    if (!url) return { error: 'url is required' }
+    const fileType = getUrlType(url)
+    const endpoint = `${this.baseUrl}/v1/summary`;
+    let res = null
+    if (fileType === 'url') {
+      res = await this._postRequest(endpoint, {
+        url,
+        chapter: false,
+        summarize: true,
+        id,
+        callback: callbackUrl
+      });
+    } else if (fileType === 'file') {
+      return { error: 'unimplemented file' }
+    } else if (fileType === 'path') {
+      const uploaded_url = await this._handle_local_file(url)
+      if (uploaded_url?.error) return uploaded_url
+      res = await this._postRequest(endpoint, {
+        external_url: false,
+        url: uploaded_url,
+        id,
+        chapter: false,
+        summarize: true,
+        callback: callbackUrl
+      });
+    }
+    let transcript = null
+    if (res?.file?.transcript) {
+      const json = await fetch(res?.file?.transcript)
+      transcript = await json.json()
+    }
+    return { transcript, summary: res?.file?.final_summary, fileId: res.file.id }
+  }
+
+  async _postRequest(endpoint, data) {
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.apiKey}`
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    console.log("POLL RESPONSE DATA responseData:", responseData)
+    if (responseData?.file?.callback) {
+      return responseData; // Asynchronous request, return initial response
+    } else {
+      return this._pollForResult(responseData.file.id); // Synchronous request, poll for result
+    }
+  }
+
+
+  async _pollForResult(fileId) {
+    console.log('poll for results!!!', fileId);
+    const pollEndpoint = `${this.baseUrl}/v1/auto/file/${fileId}?id=${fileId}`;
+    let result;
+    do {
+      const pollResponse = await fetch(pollEndpoint, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.apiKey}`
+        },
+      });
+      if (!pollResponse.ok) {
+        throw new Error(`HTTP error! Status: ${pollResponse.status}`);
+      }
+      result = await pollResponse.json();
+      if (result.error) {
+        return result;
+      }
+      if (result?.file?.failed_reason) {
+        return { error: result.file.error };
+      }
+      if (!result?.file?.complete) {
+        // Assuming the API returns a 'file' property when done. Adjust as per actual API response.
+        await new Promise(resolve => setTimeout(resolve, 3000)); // Poll every 3 seconds
+      }
+    } while (!result.file?.complete && !result?.file?.failed);
+
+    return result;
+  }
+
+  async _handle_local_file(path) {
+    if (!fs.existsSync(path)) return { error: 'file does not exist' }
+    // load the file 
+    let mime = path.split('.').pop()
+    // remove any ? query params
+    // console.log('mime', mime);
+    const endpoint = `${this.baseUrl}/v1/auto/upload/${mime}`;
+    const response = await axios.get(endpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.apiKey}`
+      },
+    });
+    const uploadURL = response.data.upload.upload;
+
+    // upload the file to the url. it's a signed s3 url 
+    const f = fs.createReadStream(path)
+    const uploadResponse = await axios.put(uploadURL, f, {
+      headers: {
+        'Content-Type': mime
+      }
+    });
+    if (uploadResponse.status !== 200) {
+      return { error: 'upload failed' }
+    }
+    return response.data.upload.url
+  }
+}
+
+module.exports = VideoSummarySDK;
